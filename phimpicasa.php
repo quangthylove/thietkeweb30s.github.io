56193bbdb7e7044357c2e3c1bc6a5ddbb3ac7fe5
Thietkeweb30s.org <honhakhanhquynh@gmail.com>
2017-03-22 11:05:03 +0700

    Add files via upload

---
+++ phimpicasa.php
@@ -0,0 +1,31 @@
+
+<script src='https://dl.dropboxusercontent.com/s/gx5lcu9n11twh60/jw6z.js'></script>
+<div id='phim_pl'></div>
+<script type='text/javascript'>
+    jwplayer('phim_pl').setup({
+        file: "","type":"mp4",
+logo:
+       {
+      file: '/get/logo01.png',
+         hide: 'false',
+         position:'top-right',
+         margin:25
+       },
+start:'0',
+          stretching: 'full',
+       abouttext: 'www.giaitrionline.org',
+       aboutlink: 'http://www.giaitrionline.org/', 
+        image: '//www.longtailvideo.com/content/images/jw-player/lWMJeVvV-876.jpg',
+        width: '100%',
+       skin: 'beelden',
+        aspectratio: '16:9',
+        autostart: 'true',
+        repeat: 'true',
+ 
+    });
+</script>
+
+<style>
+    #phim_pl{height:100%;margin:0;overflow:hidden;position:absolute;width:100%}
+body{height:100%;margin:0;overflow:hidden;position:absolute;width:100%}
+</style>
